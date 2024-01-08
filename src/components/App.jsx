71d8fa27a2453283807c94f34bfd4efceb0ed4a5
Yulia Koruma <90381665+Yulia-Koruma@users.noreply.github.com>
2024-01-08 22:31:51 +0200

    add Sorage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,12 +6,28 @@ import { ContactList } from './ContactList/ContactList';
 import { Filter } from './Filter/Filter';
 import { AppContainer, ContactsTitle, MainTitle } from './App.styled';
 
-
+const localStorageKey = 'contacts';
 export class App extends Component {
   state = { 
     contacts: [],
     filter: ''
   } 
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+      window.localStorage.setItem(localStorageKey, JSON.stringify(this.state.contacts));
+    }
+  }
+
+  componentDidMount() { 
+    const savedContacts = window.localStorage.getItem(localStorageKey);
+    
+    if (savedContacts !== null) {
+      this.setState({
+        contacts: JSON.parse(savedContacts),
+      })
+    }
+   }
 
   addContact = ({name, number}) => {
     const { contacts } = this.state;
@@ -70,3 +86,4 @@ export class App extends Component {
     );
   }
 }
+ 
